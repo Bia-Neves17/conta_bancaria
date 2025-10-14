2f3f655b143b183cbaee77b1a36267430d418f6e
Bianca Neves <bianevestorres08@gmail.com>
2025-10-14 11:05:23 -0300

    autenticação 14/10

---
+++ conta_bancaria/src/main/java/com/senai/conta_bancaria/infrastructure/config/MethodSecurityConfig.java
@@ -0,0 +1,8 @@
+package com.senai.conta_bancaria.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableMethodSecurity
+public class MethodSecurityConfig {
+}
---
+++ conta_bancaria/src/main/java/com/senai/conta_bancaria/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.senai.conta_bancaria.infrastructure.security;
+
+public class SecurityConfig {
+}
