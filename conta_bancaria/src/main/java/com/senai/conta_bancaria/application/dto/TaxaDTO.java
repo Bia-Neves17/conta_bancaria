1239b15616c1542b27656f660e001e92bfc9b2e5
Bianca Neves <bianevestorres08@gmail.com>
2025-11-18 11:07:37 -0300

    situação de aprendizagem 18/11

--- conta_bancaria/src/main/java/com/senai/conta_bancaria/application/dto/TaxaDTO.java
+++ conta_bancaria/src/main/java/com/senai/conta_bancaria/application/dto/TaxaDTO.java
@@ -1,4 +1,30 @@
 package com.senai.conta_bancaria.application.dto;
 
-public record TaxaDTO() {
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record TaxaDTO(
+
+        String id,
+
+        @NotBlank(message = "Descrição é obrigatória")
+        @Schema(description = "Descrição da taxa")
+        String descricao,
+
+        @Schema(description = "porcentagem para aplicação da taxa")
+        BigDecimal percentual,
+
+        @Schema(description = "valor fixo para aplicação da taxa")
+        BigDecimal valorFixo
+) {
+
+    @AssertTrue(message = "Informe apenas percetual ou valorFixo")
+    private boolean tipoTaxaValido(){
+    return (percentual != null && valorFixo == null) ||
+            (percentual == null && valorFixo != null);
+    }
 }
