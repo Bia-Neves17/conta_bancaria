bbe8c2476584daa12c9e9f42acc1b2500772d23f
Bianca Neves <bianevestorres08@gmail.com>
2025-11-27 09:34:14 -0300

    situação de aprendizagem 27/11

--- conta_bancaria/src/main/java/com/senai/conta_bancaria/application/service/TaxaService.java
+++ conta_bancaria/src/main/java/com/senai/conta_bancaria/application/service/TaxaService.java
@@ -1,4 +1,35 @@
 package com.senai.conta_bancaria.application.service;
 
+import com.senai.conta_bancaria.application.dto.TaxaDTO;
+import com.senai.conta_bancaria.domain.entity.Taxa;
+import com.senai.conta_bancaria.domain.repository.TaxaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TaxaService {
+
+    private final TaxaRepository taxaRepository;
+
+    public Taxa create(TaxaDTO dto){
+
+        Taxa taxa = new Taxa();
+        taxa.setDescricao(dto.descricao());
+        taxa.setPercentual(dto.percentual());
+        taxa.setValorFixo(dto.valorFixo());
+
+        return taxaRepository.save(taxa);
+    }
+
+    public List<Taxa> listAll(){return taxaRepository.findAll();}
+
+    public Taxa findById(String id){
+        return taxaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Taxa não encontrada"));
+    }
+
+    public void deletar(String id){taxaRepository.deleteById(id);}
 }
--- conta_bancaria/src/main/java/com/senai/conta_bancaria/interface_ui/controller/TaxaController.java
+++ conta_bancaria/src/main/java/com/senai/conta_bancaria/interface_ui/controller/TaxaController.java
@@ -1,4 +1,38 @@
 package com.senai.conta_bancaria.interface_ui.controller;
 
+import com.senai.conta_bancaria.application.dto.TaxaDTO;
+import com.senai.conta_bancaria.application.service.TaxaService;
+import com.senai.conta_bancaria.domain.entity.Taxa;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/taxas")
+@RequiredArgsConstructor
 public class TaxaController {
+    private final TaxaService taxaService;
+
+    @PostMapping
+    public ResponseEntity<Taxa> criar(@RequestBody TaxaDTO dto) {
+        return ResponseEntity.ok(taxaService.create(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Taxa>> findAll(){
+        return ResponseEntity.ok(taxaService.listAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taxa> findById(@PathVariable String id){
+        return ResponseEntity.ok(taxaService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable String id){
+        taxaService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
